13e2a191f405171f919f5d85b62d79a5d7b1191c
Shahal Tharique <shahalpk@gmail.com>
2022-12-09 01:11:00 +0530

    Fix: Added echo

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Always chown webroot for better mounting
+echo 'Updating owner:group of /var/www/app to nginx:nginx'
 chown -Rf nginx:nginx /var/www/app
 
 # Start supervisord and services
