3c3806ba7f7297d08150c905452b8a965e02270d
Shahal Tharique <shahalpk@gmail.com>
2022-12-25 00:29:53 +0530

    Fix: Running npm build and other cache etc

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /var/www/app
+
+npm run build
+php artisan storage:link
 php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
 
 # Update nginx to match worker_processes to no. of cpu's
 procs=$(cat /proc/cpuinfo | grep processor | wc -l)
